42825c7f8477388009bd564bcba1229fbcb1aed8
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2026-02-20 15:55:27 -0300

    atualização

--- miniMetacitric/miniMetacitric/Program.cs
+++ miniMetacitric/miniMetacitric/Program.cs
@@ -6,10 +6,13 @@ var titulos = new List<Title>();
 while (true)
 {
     Console.WriteLine("""
+
+        ________________________________
         Digite a opção desejada:
         0 - Sair        
         1 - Cadastrar um novo título
         2- Ver títulos cadastrados
+        ________________________________
         """);
     var opcao = Console.ReadLine();
 
@@ -17,12 +20,14 @@ while (true)
     {
         Console.WriteLine("""
             Novo Cadastro!
+            _________________________________________
             Se trata de um jogo, filme ou série?
             1 - Jogo
             2 - Filme
             3 - Série
+            _________________________________________
             """);
-       var cadastro = Console.ReadLine();
+        var cadastro = Console.ReadLine();
         Title novoTitulo;
         switch (cadastro)
         {
@@ -42,18 +47,18 @@ while (true)
 
                 Console.WriteLine("Qual a duração do filme, em minutos?");
                 filme.duration = int.Parse(Console.ReadLine());
-                
+
                 novoTitulo = filme;
                 break;
 
 
             case "3":
                 Console.WriteLine("Cadastrando uma nova série...");
-               var serie = new TvShow();
+                var serie = new TvShow();
 
                 Console.WriteLine("Qual a duração de cada episódio, em minutos?");
                 serie.duration = int.Parse(Console.ReadLine());
-               
+
                 novoTitulo = serie;
                 break;
 
@@ -62,13 +67,11 @@ while (true)
                 Console.WriteLine("Opção inválida. Por favor, tente novamente.");
                 return;
         }
+
         Console.WriteLine("Digite o título:");
         var nome = Console.ReadLine();
 
-        Console.WriteLine("""
-                Data de lançamento:
-                YYYY-MM-DD
-                """);
+        Console.WriteLine("Data de lançamento: (YYYY - MM - DD)");
         var dataLancamento = Console.ReadLine();
 
         Console.WriteLine("Nota dos críticos");
@@ -87,6 +90,22 @@ while (true)
         Console.WriteLine("Título cadastrado com sucesso!");
     }
 
+    void ListarTitulos()
+    {
+        if (titulos.Count == 0)
+        {
+            Console.WriteLine("Nenhum título cadastrado.");
+            return;
+        }
+
+        Console.WriteLine("===== TÍTULOS CADASTRADOS =====");
+
+        foreach (var titulo in titulos)
+        {
+            titulo.Exibir(); // Polimorfismo acontecendo aqui
+        }
+    }
+
 
     switch (opcao)
     {
@@ -95,13 +114,12 @@ while (true)
             Environment.Exit(0);
             break;
 
-            case "1":
+        case "1":
             CadastrarTitulo();
             break;
 
-            case "2":
-            Console.WriteLine($"Títulos cadastrados:");
-            titulos.ForEach(titulos => Console.WriteLine(titulos.Name));
+        case "2":
+            ListarTitulos();
             break;
         default:
             Console.WriteLine("Opção inválida. Por favor, tente novamente.");
